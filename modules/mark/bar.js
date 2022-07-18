f1077200d774c2c1fd6400e9095b6b336751fec5
Alexey Raspopov <alexeyraspopov@hotmail.com>
2022-07-18 11:59:37 -0400

    basic rendering algorithm and bar mark

---
+++ modules/mark/bar.js
@@ -0,0 +1,57 @@
+import { scaleLinear, scaleBand } from "d3-scale";
+
+export function barX(data, encodings) {
+  let contdomain = encodings.domain.y;
+  let cont = scaleLinear(contdomain, [2 ** 16, 0]);
+  let orddomain = data.map((datum) => datum[encodings.x]);
+  let ord = scaleBand(orddomain, [0, 2 ** 16]).padding(0.1);
+  return {
+    key: "bar",
+    shapes: [
+      {
+        tag: "rect",
+        attrs: {
+          x: "x(d.x)",
+          y: "y(d.y)",
+          width: `x(${ord.bandwidth()})`,
+          height: "y(2 ** 16 - d.y)",
+          fill: "'currentColor'",
+        },
+      },
+    ],
+    channels: {
+      index: Uint16Array.from(data, (_, index) => index),
+      x: Float32Array.from(data, (datum) => ord(datum[encodings.x])),
+      y: Float32Array.from(data, (datum) => cont(datum[encodings.y])),
+      fill: [], // can be cont or ord
+    },
+  };
+}
+
+export function barY(data, encodings) {
+  let contdomain = encodings.domain.x;
+  let cont = scaleLinear(contdomain, [0, 2 ** 16]);
+  let orddomain = data.map((datum) => datum[encodings.y]);
+  let ord = scaleBand(orddomain, [2 ** 16, 0]).padding(0.1);
+  return {
+    key: "bar",
+    shapes: [
+      {
+        tag: "rect",
+        attrs: {
+          x: 0,
+          y: "y(d.y)",
+          width: `x(d.x)`,
+          height: `y(${ord.bandwidth()})`,
+          fill: "'currentColor'",
+        },
+      },
+    ],
+    channels: {
+      index: Uint16Array.from(data, (_, index) => index),
+      x: Float32Array.from(data, (datum) => cont(datum[encodings.x])),
+      y: Float32Array.from(data, (datum) => ord(datum[encodings.y])),
+      fill: [], // can be cont or ord
+    },
+  };
+}
--- modules/plot.js
+++ modules/plot.js
@@ -1,3 +1,57 @@
-export function plot(children) {
-  throw new Error("implement me");
+import { scaleLinear } from "d3-scale";
+import { create, select } from "d3-selection";
+
+export function plot(root, layers) {
+  let rect = root.getBoundingClientRect();
+
+  let x = scaleLinear([0, 2 ** 16], [0, rect.width]);
+  let y = scaleLinear([0, 2 ** 16], [0, rect.height]);
+
+  let key = (datum, index) => (datum != null ? datum.key : index);
+  let children = select(root).selectAll((_, index, nodes) => nodes[index].children);
+
+  return children.data(layers, key).join(
+    (enter) => {
+      enter.append((layer) => {
+        let target = create("svg:g").attr("data-key", layer.key);
+        let children = target.selectAll((_, index, nodes) => nodes[index].children);
+
+        children.data(layer.channels.index).join(
+          (enter) => {
+            for (let shape of layer.shapes) {
+              enter.append((pointer) => {
+                let target = create("svg:" + shape.tag);
+                target.attr("data-key", pointer);
+
+                let access = new Proxy({}, { get: (_, key) => layer.channels[key][pointer] });
+                for (let key in shape.attrs) {
+                  let expr = shape.attrs[key];
+                  let fn = new Function("x", "y", "d", "return " + expr);
+                  target.attr(key, fn(x, y, access));
+                }
+
+                return target.node();
+              });
+            }
+          },
+          (update) => {
+            update.each(function () {});
+          },
+          (exit) => {},
+        );
+
+        return target.node();
+      });
+    },
+    (update) => {
+      update.each(function (layer) {
+        console.log("update", this, layer);
+      });
+    },
+    (exit) => {
+      exit.each(function (layer) {
+        console.log("exit", this, layer);
+      });
+    },
+  );
 }
