1c14ef5d2067683002f7689157c897c1041fc4e8
Alexey Raspopov <alexeyraspopov@hotmail.com>
2022-07-26 23:03:12 -0400

    implement dot mark

--- modules/mark/dot.js
+++ modules/mark/dot.js
@@ -1,3 +1,33 @@
+import { extent } from "d3-array";
+import { scaleLinear } from "d3-scale";
+
 export function dot(data, encodings) {
-  throw new Error("implement me");
+  let h = scaleLinear(
+    extent(data, (datum) => datum[encodings.x]),
+    [0, 2 ** 16],
+  );
+  let v = scaleLinear(
+    extent(data, (datum) => datum[encodings.y]),
+    [2 ** 16, 0],
+  );
+  return {
+    key: "dot",
+    shapes: [
+      {
+        tag: "circle",
+        attrs: {
+          cx: "x(d.x)",
+          cy: "y(d.y)",
+          r: "2",
+          stroke: "'currentColor'",
+          fill: "'transparent'",
+        },
+      },
+    ],
+    channels: {
+      index: Uint16Array.from(data, (_, index) => index),
+      x: Float32Array.from(encodings.values(data, encodings.x), h),
+      y: Float32Array.from(encodings.values(data, encodings.y), v),
+    },
+  };
 }
