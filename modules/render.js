422133adf353f7557c770c37c5400221b9d71ca1
Alexey Raspopov <alexeyraspopov@hotmail.com>
2022-11-17 11:18:50 -0500

    barebones render

--- modules/render.js
+++ modules/render.js
@@ -1,4 +1,3 @@
-import { create, select } from "./d3-selection.js";
 import { Linear } from "./scale/scale.js";
 
 /**
@@ -14,45 +13,26 @@ export function render(blueprint, root, style) {
   let paddingY = style?.paddingY ?? 0;
   let x = Linear([0, 2 ** 16], [0 + paddingX, rect.width - paddingX]);
   let y = Linear([0, 2 ** 16], [0 + paddingY, rect.height - paddingY]);
-  let children = select(root).selectAll((_, index, nodes) => nodes[index].children);
-  let key = (datum, index) => (datum != null ? datum.key : index);
-
-  return children.data(blueprint.layers, key).join(
-    (enter) => {
-      enter.append((layer) => {
-        let target = create("svg:g").attr("data-key", layer.key);
-        let children = target.selectAll((_, index, nodes) => nodes[index].children);
-
-        children.data(layer.channels.index).join(
-          (enter) => {
-            // QUESTION can I express layer.shapes in terms of selection
-            for (let shape of layer.shapes) {
-              enter.append((pointer) => {
-                let target = create("svg:" + shape.tag).attr("data-key", pointer);
-                let scope = { x, y, d: getDatumReaderBy(pointer, layer.channels) };
-                for (let key in shape.attrs) {
-                  let expr = shape.attrs[key];
-                  let fn = new Function("scope", "with (scope) return " + expr);
-                  target.attr(key, fn(scope));
-                }
-                return target.node();
-              });
-            }
-          },
-          (update) => {},
-          (exit) => {},
-        );
-
-        return target.node();
-      });
-    },
-    (update) => {
-      console.log(update);
-    },
-    (exit) => {
-      console.log(exit);
-    },
-  );
+  let layout = document.createDocumentFragment();
+  for (let layer of blueprint.layers) {
+    let group = createElement("g", "svg");
+    for (let pointer of layer.channels.index) {
+      let fragment = document.createDocumentFragment();
+      for (let shape of layer.shapes) {
+        let node = createElement(shape.tag, "svg");
+        let scope = { x, y, d: getDatumReaderBy(pointer, layer.channels) };
+        for (let key in shape.attrs) {
+          let expr = shape.attrs[key];
+          let fn = new Function("scope", "with (scope) return " + expr);
+          node.setAttribute(key, fn(scope));
+        }
+        fragment.appendChild(node);
+      }
+      group.appendChild(fragment);
+    }
+    layout.appendChild(group);
+  }
+  root.appendChild(layout);
 }
 
 function getDatumReaderBy(pointer, channels) {
@@ -62,4 +42,15 @@ function getDatumReaderBy(pointer, channels) {
       else throw new Error("Unable to find channel " + key);
     },
   });
+}
+
+var uris = {
+  svg: "http://www.w3.org/2000/svg",
+  xhtml: "http://www.w3.org/1999/xhtml",
+  xlink: "http://www.w3.org/1999/xlink",
+  xml: "http://www.w3.org/XML/1998/namespace",
+  xmlns: "http://www.w3.org/2000/xmlns/",
+};
+function createElement(name, type) {
+  return type == null ? document.createElement(name) : document.createElementNS(uris[type], name);
 }
