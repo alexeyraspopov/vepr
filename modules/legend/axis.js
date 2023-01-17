768dfe2a3bade2ffb5555de8fcde967e584175a6
Alexey Raspopov <alexeyraspopov@hotmail.com>
2023-01-16 23:50:48 -0500

    improved tick overlap filter

--- modules/legend/axis.js
+++ modules/legend/axis.js
@@ -21,6 +21,7 @@ export function axisX(variable) {
       text: ticks,
       textAlign: "center",
       textBaseline: "middle",
+      filterStrategy: "parity",
     },
   };
 }
@@ -45,6 +46,7 @@ export function axisY(variable) {
       text: ticks,
       textAlign: "right",
       textBaseline: "middle",
+      filterStrategy: "parity",
     },
   };
 }
@@ -66,36 +68,45 @@ export function renderAxis(context, scales, channels) {
   let labels = Array.from(channels.index, (pointer) => {
     let text = channels.text[pointer];
     let metric = context.measureText(text);
-    return { text, width: metric.width, x: x(pointer), y: y(pointer) };
+    let x1 = x(pointer);
+    let y1 = y(pointer);
+    let x2 = x1 + metric.width;
+    let y2 = y1 + metric.actualBoundingBoxAscent + metric.actualBoundingBoxDescent;
+    return { text, x1, x2, y1, y2 };
   });
 
+  let M = context.measureText("M");
+  let filter = channels.filterStrategy === "greedy" ? greedy : parity;
+
   if (ArrayBuffer.isView(channels.x)) {
-    let gap = context.measureText("M").width;
-    while (hasTextOverlapX(labels, gap)) {
-      labels = labels.filter((_, index) => index % 2 === 0);
-    }
+    let gap = M.width;
+    labels = filter(labels, gap);
   }
 
   if (ArrayBuffer.isView(channels.y)) {
-    let gap = context.measureText("M").emHeightAscent * 2;
-    while (hasTextOverlapY(labels, gap)) {
-      labels = labels.filter((_, index) => index % 2 === 0);
-    }
+    let gap = M.actualBoundingBoxAscent + M.actualBoundingBoxDescent;
+    labels = filter(labels, gap);
   }
 
   for (let label of labels) {
-    context.fillText(label.text, label.x, label.y);
+    context.fillText(label.text, label.x1, label.y1);
   }
 }
 
-function hasTextOverlapX(labels, gap) {
-  return labels.some((label, index, all) => {
-    return index < all.length - 1 && label.x + label.width + gap >= all[index + 1].x;
-  });
+function parity(labels, gap) {
+  while (labels.some((l, i, ls) => i > 0 && intersect(l, ls[i - 1], gap))) {
+    labels = labels.filter((_, index) => index % 2 === 0);
+  }
+  return labels;
 }
 
-function hasTextOverlapY(labels, gap) {
-  return labels.some((label, index, all) => {
-    return index > 0 && label.y + gap >= all[index - 1].y - gap;
-  });
+function greedy(labels, gap) {
+  let cursor;
+  return labels.filter(
+    (label) => (cursor == null || !intersect(label, cursor, gap)) && ((cursor = label), true),
+  );
+}
+
+function intersect(a, b, gap = 0) {
+  return gap > Math.max(b.x1 - a.x2, a.x1 - b.x2, b.y1 - a.y2, a.y1 - b.y2);
 }
